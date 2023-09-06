6dca9d5d1b321c169e544307fac32f0aa06f11bd
Zach Smith <zwsmith27@gmail.com>
2023-09-06 13:59:24 -0700

    fixed typo in models

--- backend/db/seeders/20230906200720-user.js
+++ backend/db/seeders/20230906200720-user.js
@@ -10,27 +10,27 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-const userNames = ['Demo-lition', 'FakeUser1', 'FakeUser2']
+const userSeeds = [
+  {
+    email: 'demo@user.io',
+    username: 'Demo-lition',
+    hashedPassword: bcrypt.hashSync('password')
+  },
+  {
+    email: 'user1@user.io',
+    username: 'FakeUser1',
+    hashedPassword: bcrypt.hashSync('password2')
+  },
+  {
+    email: 'user2@user.io',
+    username: 'FakeUser2',
+    hashedPassword: bcrypt.hashSync('password3')
+  }
+]
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
-      {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-    ], { validate: true });
+    await User.bulkCreate(userSeeds, {validate: true});
   },
 
   async down (queryInterface, Sequelize) {
