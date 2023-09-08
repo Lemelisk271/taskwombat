7919e91f47cb488ceb52952618d4b7f67ad83082
Zach Smith <zwsmith27@gmail.com>
2023-09-08 08:31:06 -0700

    added categories

---
+++ backend/db/seeders/20230908150816-category.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { Category } = require('../models')
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+const categoryList = [
+  "Handyman",
+  "Moving Services",
+  "Furniture Assembly",
+  "Mounting & Installation",
+  "Cleaning",
+  "Shopping & Delivery",
+  "Yardwork Services",
+  "Holidays",
+  "Winter Tasks",
+  "Personal Assistant",
+  "Baby Prep",
+  "Virtual & Online Tasks",
+  "Parties & Events",
+  "Office Services",
+  "Contactless Tasks"
+]
+
+const categorySeeds = []
+
+categoryList.forEach(el => {
+  const cat = {
+    category: el
+  }
+  console.log(cat)
+  categorySeeds.push(cat)
+})
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Category.bulkCreate(categorySeeds, {validate: true})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Categories';
+    return queryInterface.bulkDelete(options)
+  }
+};
