3874a81d01c356db41090c7bbe0b4888288e1ebd
Zach Smith <zwsmith27@gmail.com>
2023-09-09 09:26:16 -0700

    create the vehicle table

---
+++ backend/db/models/vehicle.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vehicle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Vehicle.init({
+    vehicleType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Vehicle',
+    defaultScope: {
+      attributes: {
+        exclude: ["createdAt", "updatedAt"]
+      }
+    }
+  });
+  return Vehicle;
+};
