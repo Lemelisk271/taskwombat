ba824a4b2abe8f8978c03680846f8cf87233555e
Zach Smith <zwsmith27@gmail.com>
2023-09-11 15:30:53 -0700

    updated the style for the user review list item

--- frontend/src/components/UserReviewListItem/UserReviewListItem.js
+++ frontend/src/components/UserReviewListItem/UserReviewListItem.js
@@ -40,7 +40,7 @@ const UserReviewListItem = ({ review }) => {
 
   return (
     <div className="UserReviewListItem">
-      <div className='userReviewListItem-line'></div>
+      {/* <div className='userReviewListItem-line'></div> */}
       <div className='userReviewListItem-card'>
         <Link to={`/taskers/${review.Tasker.id}`} className='userReviewListItem-cardHead'>
           <img src={review.Tasker.profileImage} alt="Tasker"/>
@@ -92,7 +92,7 @@ const UserReviewListItem = ({ review }) => {
           />
         </div>
       </div>
-      <div className='userReviewListItem-line'></div>
+      {/* <div className='userReviewListItem-line'></div> */}
     </div>
   )
 }
