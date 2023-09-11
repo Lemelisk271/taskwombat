fc7f21554508791b14c6c1e2891c4aa68bd973f8
Zach Smith <zwsmith27@gmail.com>
2023-09-11 13:26:35 -0700

    created an image page for testing

--- frontend/src/components/Images/Images.js
+++ frontend/src/components/Images/Images.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react'
+
 const Images = () => {
+  const [reviewImages, setReviewImages] = useState([])
+
+  useEffect(() => {
+    const loadPage = async () => {
+      const images = await fetch('/api/images')
+      const data = await images.json()
+      setReviewImages(data)
+    }
+    loadPage()
+  }, [])
+
   return (
     <div className='images'>
-      <h1>Images</h1>
+      {reviewImages.map(image => (
+        <p>{image.id}</p>
+      ))}
     </div>
   )
 }
