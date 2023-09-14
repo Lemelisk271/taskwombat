26c8d380426ef16e3d714390f2a7edd0f6e8c56f
Zach Smith <zwsmith27@gmail.com>
2023-09-14 16:33:09 -0700

    added the trust and support page and style

--- frontend/src/components/FeeModal/FeeModal.js
+++ frontend/src/components/FeeModal/FeeModal.js
@@ -1,7 +1,21 @@
+import './FeeModal.css'
+
 const FeeModal = () => {
   return (
-    <div className="FeeModal">
-      <h1>Fee Modal</h1>
+    <div className="feeModal">
+      <h1>What's the TaskWombat Trust and Support Fee?</h1>
+      <p>TaskWombat applies a trust and support fee to all invoices. Expenses and reimbursements are <span>not</span> subject to this fee. You'll see this fee as a separate item on your receipt once the task is complete.</p>
+
+      <div className='feeModal-list'>
+        <p>This fee helps to support:</p>
+        <ul>
+          <li>Operational and safety measures to protect users.</li>
+          <li>Investments in our Customer Support Team</li>
+          <li>Tools, team training, and channels to support you in getting your task completed.</li>
+        </ul>
+      </div>
+
+      <p>Don't worry -- the Trust & Support fee doesn't effect what the tasker id paid.</p>
     </div>
   )
 }
