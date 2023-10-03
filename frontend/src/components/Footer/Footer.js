c6d96c90d6615d2e8db6ccf55af13467435f1312
Zach Smith <zwsmith27@gmail.com>
2023-10-03 12:46:28 -0700

    fixed about links and added portfolio

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,14 +5,18 @@ const Footer = () => {
 
   return (
     <div className="footer">
+      <p>&copy; {year} Zach Smith, All rights reserved</p>
       <a href="https://github.com/Lemelisk271" target="_blank" rel="noreferrer" className="footer-github">
-        <i className="fa-brands fa-github"></i>
+        <i className="fa-brands fa-square-github"></i>
         <p>GitHub</p>
       </a>
-      <p>&copy; {year} Zach Smith, All rights reserved</p>
-      <a href="www.linkedin.com/in/zwsmith27" target="_blank" rel="noreferrer" className="footer-github">
+      <a href="https://www.linkedin.com/in/zwsmith27" target="_blank" rel="noreferrer" className="footer-github">
         <i className="fa-brands fa-linkedin"></i>
         <p>LinkedIn</p>
+      </a>
+      <a href="https://portfolio-sugg.onrender.com/" target="_blank" rel="noreferrer" className="footer-github">
+        <i className="fa-solid fa-house"></i>
+        <p>Portfolio</p>
       </a>
     </div>
   )
